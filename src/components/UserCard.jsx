04dbc3b47460015c4c34623c0835e46c2c9e5d12
Elena Rotari <elena.rotari@icloud.com>
2023-08-16 16:28:48 +0200

    implimented functionality to get a name and desc for public repos

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import { useActionData } from "react-router-dom";
-
+import "./userCard.scss";
 const UserCard = () => {
-  const user = useActionData();
-  console.log(user);
-  return <div>user</div>;
+  const data = useActionData();
+  const { userData, repos } = data;
+  console.log(data);
+
+  return (
+    <div className="card">
+      <h1>{userData?.name}</h1>
+      <img src={userData?.avatar_url} alt={userData.name} />
+      <h4>{userData?.public_repos}</h4>
+      {repos?.map((repo) => (
+        <div key={repo.id}>
+          <h3>
+            <span>Repositorium Name: </span>
+            {repo.name}
+          </h3>
+          <h4>
+            <span>Description:</span>
+            {repo.description
+              ? repo.description
+              : " Unfortunately, there is no description for this repository"}
+          </h4>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default UserCard;
@@ -14,5 +36,7 @@ export const action = async ({ request }) => {
   const name = data.get("user");
   const getUserData = await fetch(`https://api.github.com/users/${name}`);
   const userData = await getUserData.json();
-  return userData;
+  const getRepos = await fetch(userData.repos_url);
+  const repos = await getRepos.json();
+  return { userData, repos };
 };
